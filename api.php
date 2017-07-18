5b4f1719eeee7668c716e7ab6b90790192bb6dbf
kviatkovsky <kviatkovsky.v@gmail.com>
2017-07-18 10:58:56 +0300

    Update api.php

--- api.php
+++ api.php
@@ -1,48 +1,47 @@
 <?php
-
-    $url = "http://suitecrm.zzz.com.ua/service/v2/soap.php?wsdl";
-    $username = "vk";
-    $password = "heiniken526";
-
-    //require NuSOAP
-    require_once("nusoap.php");
-
-    //retrieve WSDL
-    $client = new nusoap_client($url, 'wsdl');
-
-    //display errors
-    $err = $client->getError();
-    if ($err)
+    $url = "http://suitecrm.zzz.com.ua/service/v4_1/rest.php";
+    $username = "vk"; 
+    $password = "heiniken526"; 
+  
+    function call($method, $parameters, $url)
     {
-        echo '<h2>Constructor error</h2><pre>' . $err . '</pre>';
-        echo '<h2>Debug</h2><pre>' . htmlspecialchars($client->getDebug(), ENT_QUOTES) . '</pre>';
-        exit();
+        ob_start();
+        $curl_request = curl_init();
+        curl_setopt($curl_request, CURLOPT_URL, $url);
+        curl_setopt($curl_request, CURLOPT_POST, 1);
+        curl_setopt($curl_request, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_0);
+        curl_setopt($curl_request, CURLOPT_HEADER, 1);
+        curl_setopt($curl_request, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($curl_request, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl_request, CURLOPT_FOLLOWLOCATION, 0);
+        $jsonEncodedData = json_encode($parameters);
+        $post = array(
+             "method" => $method,
+             "input_type" => "JSON",
+             "response_type" => "JSON",
+             "rest_data" => $jsonEncodedData
+        );
+        curl_setopt($curl_request, CURLOPT_POSTFIELDS, $post);
+        $result = curl_exec($curl_request);
+        curl_close($curl_request);
+        $result = explode("\r\n\r\n", $result, 2);
+        $response = json_decode($result[1]);
+        ob_end_flush();
+        return $response;
     }
-
-    //login ------------------------------------------------ 
+    //login --------------------------------------------
     $login_parameters = array(
-         'user_auth' => array(
-              'user_name' => $username,
-              'password' => md5($password),
-              'version' => '1'
+         "user_auth"=>array(
+              "user_name"=>$username,
+              "password"=>md5($password),
+              "version"=>"1"
          ),
-         'application_name' => 'SoapTest',
-         'name_value_list' => array(
-         ),
+         "application_name"=>"RestTest",
+         "name_value_list"=>array(),
     );
-
-    $login_result = $client->call('login', $login_parameters);
-
-    /*
-    echo '<pre>';
-    print_r($login_result);
-    echo '</pre>';
-    */
-
-    //get session id
-    $session_id =  $login_result['id'];
-
-    //get list of records ---------------------------------- 
+    $login_result = call("login", $login_parameters, $url);
+   
+    $session_id = $login_result->id;
     $params = array(
         "Leads"=>'first_name',
         "Contacts"=>'first_name',
@@ -51,77 +50,52 @@
         "Opportunities"=>'name',
         "Users"=>'user_name',  
         );
-    // var_dump($params);
-    // echo "<br>";
-    // echo "<br>";
-    function getEntry($session_id,$key,$value){
-
-    //      var_dump($params);
-    // echo "<br>";
-    // echo "<br>";
-        $get_entry_list_parameters = array(
-
-                //session id
-                'session' => $session_id,
-
-                //The name of the module from which to retrieve records
-                'module_name' => $key,
-
-                //The SQL WHERE clause without the word "where".
-                'query' => "",
-
-                //The SQL ORDER BY clause without the phrase "order by".
-                'order_by' => "",
-
-                //The record offset from which to start.
-                'offset' => '0',
-
-                //Optional. A list of fields to include in the results.
-                'select_fields' => array(
-                    'id',
-                     $value,
-                    'last_name',
-                ),
-
-                /*
-                A list of link names and the fields to be returned for each link name.
-                Example: 'link_name_to_fields_array' => array(array('name' => 'email_addresses', 'value' => array('id', 'email_address', 'opt_out', 'primary_address')))
-                */
-                'link_name_to_fields_array' => array(
-                ),
-
-                //The maximum number of results to return.
-                'max_results' => '20',
-
-                //To exclude deleted records
-                'deleted' => '0',
-
-                //If only records marked as favorites should be returned.
-                'Favorites' => false,
-            );
-           
-        return $get_entry_list_parameters;
-    }
+    
+   function getEntry($session_id,$key,$value){
+    $get_entry_list_parameters = array(
+         
+         'session' => $session_id,
+        
+         'module_name' => $key,
+        
+        
+         'query' => "",
+        
+         'order_by' => "",
+        
+         'offset' => '0',
   
+         'select_fields' => array(
+              'id',
+               $value,
+              'last_name',
+         ),
+         
+         'link_name_to_fields_array' => array(
+         ),
+         
+         'max_results' => '20',
+         
+         'deleted' => '0',
+    
+         'Favorites' => false,
+    );
+    return $get_entry_list_parameters;
+    }
     foreach($params as $key => $value){
-        // print_r($value);
-        echo "<br>";
+    $get_entry_list_result = call('get_entry_list', getEntry($session_id,$key,$value), $url);
+    echo $key.'<hr>';
+    for($i = 0; $i<10; $i++){
     
-   
-    $get_entry_list_result = $client->call('get_entry_list', getEntry($session_id,$key,$value));
-
-          echo $key.":<hr>";
-    for($i = 0; $i<count($get_entry_list_result['entry_list']); $i++){
-        echo $get_entry_list_result['entry_list'][$i]['name_value_list'][1]['value'].' '.$get_entry_list_result['entry_list'][$i]['name_value_list'][2]['value'].'<br>';
-        }
+   $get_entry_list_result = (array)$get_entry_list_result;
+   echo "<br>";
     
-
-
-
-    // echo '<pre>';
-    // print_r($get_entry_list_result['entry_list']);
-    // echo '</pre>';
-   }
-
-
+    echo $get_entry_list_result['entry_list'][$i]->name_value_list->name->value;
+    echo $get_entry_list_result['entry_list'][$i]->name_value_list->first_name->value;
+    echo " ".$get_entry_list_result['entry_list'][$i]->name_value_list->last_name->value;
+    echo " ".$get_entry_list_result['entry_list'][$i]->name_value_list->user_name->value;
+          
+     }
+     echo '<hr>';
+}
 ?>
